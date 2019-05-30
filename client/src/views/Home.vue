80ec0e0c1da0c2e14cbad61d2347a6782268ac2a
Olaoluwa Faniyi <gofaniyi@gmail.com>
2019-05-31 00:14:30 +0100

    Implemented the following - LandingPage component - Home page to import the LandingPage component - Designed the authentication system for VueJs application - Developed and connected authentication with Flask API backend

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <span>
+        <landing-page></landing-page>
+    </span>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import LandingPage from '@/components/LandingPage';
+import store from '@/store.js';
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    components: {
+        LandingPage,
+    }
 };
 </script>

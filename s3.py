2d78e714b764f5ab757bb5e2a9bdb4a99fe2484b
Olaoluwa Faniyi <gofaniyi@gmail.com>
2019-06-21 10:17:45 +0100

    Running another script file to push data to s3

---
+++ s3.py
@@ -0,0 +1,28 @@
+import flask_s3
+
+from collections import namedtuple
+
+MyStruct = namedtuple('MyStruct', 'config static_folder static_url_path')
+
+def upload():
+    """
+    upload static files to s3
+
+    Return:
+        func: call the function if successful or the click help option if unsuccesful
+    """
+    print('Uploading static files to S3....')
+    params = { 
+        'config' : {
+            'FLASKS3_BUCKET_NAME': 'zappa-ok587zsna',
+        },
+        'static_folder' : './dist/static',
+        'static_url_path' : '/static'
+    }
+    app = MyStruct(**params)
+    flask_s3.create_all(app)
+
+    print('Uploaded static files to S3.....')
+
+
+upload()
